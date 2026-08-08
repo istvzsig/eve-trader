5a891a2af56657fcefc215410afc692a42c33cd4
istvzsig <istvanzsigmond@aol.com>
2026-08-08 14:06:49 +0200

    feat: update ISK parser to use string

--- internal/parse/isk.go
+++ internal/parse/isk.go
@@ -1,9 +1,32 @@
 package parse
 
-func ISK(value float64) float64 {
-	return value * 1000
-}
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
-func ISKMillion(value float64) float64 {
-	return value * 1000000
+func ISK(value string) (float64, error) {
+	value = strings.TrimSpace(strings.ToLower(value))
+
+	multiplier := 1.0
+
+	switch {
+	case strings.HasSuffix(value, "k"):
+		multiplier = 1_000
+		value = strings.TrimSuffix(value, "k")
+	case strings.HasSuffix(value, "m"):
+		multiplier = 1_000_000
+		value = strings.TrimSuffix(value, "m")
+	case strings.HasSuffix(value, "b"):
+		multiplier = 1_000_000_000
+		value = strings.TrimSuffix(value, "b")
+	}
+
+	number, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		return 0, fmt.Errorf("invalid ISK: %q", value)
+	}
+
+	return number * multiplier, nil
 }
